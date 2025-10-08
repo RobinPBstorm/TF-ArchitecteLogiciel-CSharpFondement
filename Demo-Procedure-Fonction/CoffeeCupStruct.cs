4acbb2b67660d2bc94cce57641dc348799c09378
robinPecheurBstorm <robin.pecheur@bstorm.be>
2025-10-08 12:49:20 +0200

    Demo sur les fonctions et les procédures

---
+++ Demo-Procedure-Fonction/CoffeeCupStruct.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Procedure_Fonction
+{
+    public struct CoffeeCupStruct
+    {
+        public uint volume;
+
+        public CoffeeCupStruct(uint volume)
+        {
+            this.volume = volume;
+        }
+        public CoffeeCupStruct(int volume)
+        {
+            if (volume >= 0)
+            {
+                this.volume = (uint)volume;
+            }
+            else
+            {
+                this.volume = 0;
+            }
+        }
+
+        // procédure
+        public void Fill(uint volume)
+        {
+            volume *= 2;
+            this.volume = volume;
+        }
+        // paramètre optionnel => les derniers défini
+        // posséde une valeur par defaut
+        public void FillWithOptional(uint volume = 5)
+        {
+            this.volume = volume;
+        }
+        public void FillRef(ref uint volume)
+        {
+            volume *= 2;
+            this.volume = volume;
+        }
+        public void FillWithParams(params uint[] volumes)
+        {
+            foreach (uint volume in volumes)
+            {
+                this.volume += volume;
+            }
+        }
+
+
+        // Fonction
+        public bool isEmpty()
+        {
+            return volume == 0;
+        }
+        // surcharge = même nom mais les mêmes paramètres (type et nombre)
+        // signature = nom + paramètre
+        public bool TryEmpty(uint volumeToRemove, out uint finalVolume)
+        {
+            if (volume >= volumeToRemove)
+            {
+                volume -= volumeToRemove;
+                finalVolume = volume;
+                return true;
+            }
+            else
+            {
+                finalVolume = volume;
+                return false;
+            }
+        }
+        public bool TryEmpty(uint volumeToRemove, out uint finalVolume, out string message)
+        {
+            if (volume >= volumeToRemove)
+            {
+                volume -= volumeToRemove;
+                finalVolume = volume;
+                message = "C'était bien bon";
+                return true;
+            }
+            else
+            {
+                finalVolume = volume;
+                message = "Il n'y en avait pas assez";
+                return false;
+            }
+        }
+    }
+}
---
+++ Demo-Procedure-Fonction/Program.cs
@@ -0,0 +1,28 @@
+﻿using Demo_Procedure_Fonction;
+
+CoffeeCupStruct myCup = new CoffeeCupStruct(2);
+
+Console.WriteLine($"Le volume de départ est {myCup.volume}");
+
+uint teaVolume = 5;
+//myCup.Fill(teaVolume);
+//myCup.FillRef(ref teaVolume);
+myCup.FillWithOptional(8);
+
+// passer un paramètre spécifique avec son nom
+myCup.FillWithOptional(volume: 8);
+
+myCup.FillWithParams(8,5,8);
+
+Console.WriteLine($"Le volume après notre procédure est {myCup.volume}");
+Console.WriteLine(myCup.isEmpty() ? "Ma tasse es vide": "Il y a quelque chose dans ma tasse");
+Console.WriteLine($"Mon volume est de {teaVolume}");
+
+uint volumeLeft;
+if (myCup.TryEmpty(5, out volumeLeft, out string message))
+{
+    Console.WriteLine(message);
+    Console.WriteLine(volumeLeft);
+}
+
+
